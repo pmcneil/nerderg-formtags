4baa4c065da5085d56aa6afb89cd20bf3dcaa29a
pmcneil <peter@mcneils.net>
2012-11-18 15:20:14 +1100

    Change the plugin name to match the older version

--- NerdergFormTagsGrailsPlugin.groovy
+++ NerdergFormTagsGrailsPlugin.groovy
@@ -1,4 +1,4 @@
-class NerdergFormtagsGrailsPlugin {
+class NerdergFormTagsGrailsPlugin {
     // the plugin version
     def version = "2.0"
     // the version or versions of Grails the plugin is designed for
